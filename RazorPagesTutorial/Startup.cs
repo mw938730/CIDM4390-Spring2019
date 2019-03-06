fc80ff23ec93625b3b3fc90ab5b2923ad25984e5
jeff <ahuimanu@gmail.com>
2019-03-05 22:35:25 -0600

    updated for azure pub

--- RazorPagesTutorial/Startup.cs
+++ RazorPagesTutorial/Startup.cs
@@ -45,6 +45,10 @@ namespace RazorPagesTutorial
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+
+            app.UseDeveloperExceptionPage();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
