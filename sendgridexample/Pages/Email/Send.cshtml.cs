c2d78046da6eb3a6d74d112658226a8026a8263c
jeff <ahuimanu@gmail.com>
2019-03-26 00:15:26 -0500

    completed sendgrid example

--- sendgridexample/Pages/Email/Send.cshtml.cs
+++ sendgridexample/Pages/Email/Send.cshtml.cs
@@ -5,12 +5,69 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyApp.Namespace
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+using sendgridexample.Models;
+
+namespace sendgridexample.Email
 {
     public class SendModel : PageModel
     {
+
+        public async Task<string> SendEmail(string email, string subject, string message)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("jeff@babbster.net", "Babbster");
+            //var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress(email, "The Dude");
+            var plainTextContent = message;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, " ");
+            
+            var response = await client.SendEmailAsync(msg);
+
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Body.ReadAsStringAsync().Result); // The message will be here
+            Console.WriteLine(response.Headers.ToString());
+
+            return response.StatusCode.ToString();
+        }
+
+        [BindProperty]
+        public EmailModel Email{ get; set;}
+
         public void OnGet()
         {
+
+        }
+
+        // public IActionResult OnPost()
+        // {
+        //     if(!ModelState.IsValid)
+        //     {
+        //         return Page();
+        //     }
+
+        //     return RedirectToPage("/Email/Result");
+        // }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var status = await SendEmail(Email.Email, Email.Subject, Email.Message);
+
+            TempData["ResponseCode"] = status;
+            TempData["EmailAddress"] = Email.Email;
+            TempData["EmailSubject"] = Email.Subject;
+            TempData["EmailMessage"] = Email.Message;            
+
+            return RedirectToPage("/Email/Result");            
+
         }
     }
 }
--- sendgridexample/Startup.cs
+++ sendgridexample/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using dotenv.net;
+
 namespace sendgridexample
 {
     public class Startup
@@ -31,6 +33,13 @@ namespace sendgridexample
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            // configure dotenv
+            // services.AddEnv(builder => {
+            //     builder
+            //         .AddThrowOnError(false)
+            //         .AddEncoding(Encoding.ASCII);
+            // });            
+
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -48,6 +57,8 @@ namespace sendgridexample
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            DotEnv.Config();            
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
